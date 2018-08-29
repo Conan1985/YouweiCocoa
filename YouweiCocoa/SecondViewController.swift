2945fbe1a64c500aade53b9b6364b0bab205aee9
Conan1985 <Conan1985@users.noreply.github.com>
2018-08-29 16:33:32 -0400

    move alamofire to second view

--- YouweiCocoa/SecondViewController.swift
+++ YouweiCocoa/SecondViewController.swift
@@ -6,14 +6,49 @@
 //  Copyright © 2018 Timothy Masterson. All rights reserved.
 //
 
+import Alamofire
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    private static var manager: Alamofire.SessionManager = {
+        
+        // Create the server trust policies
+        let serverTrustPolicies: [String: ServerTrustPolicy] = [
+            "aggregation-web.sit.va.anthem.com": .disableEvaluation
+        ]
+        
+        // Create custom manager
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = Alamofire.SessionManager.defaultHTTPHeaders
+        let manager = Alamofire.SessionManager(
+            configuration: URLSessionConfiguration.default,
+            serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies)
+        )
+        
+        return manager
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let headers: HTTPHeaders = [
+            "X-MADT-AppId": "ABCBS",
+            "X-MADT-AppVersion": "8.0.1200",
+            "Content-Type": "application/json"
+        ]
+        let parameters: Parameters = [
+            "userName": "SIT5SUB340T91519",
+            "password": "support1",
+            "actualUserName": ""
+        ]
+        SecondViewController.manager.request("https://aggregation-web.sit.va.anthem.com/ma-authentication-app-v6/rest/public/login", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- YouweiCocoa/ViewController.swift
+++ YouweiCocoa/ViewController.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2018 Youwei Lu. All rights reserved.
 //
 
-import Alamofire
 import UIKit
 
 class ViewController: UIViewController {
@@ -19,26 +18,6 @@ class ViewController: UIViewController {
     var username:String = ""
     
     @IBOutlet weak var usernameLabel:UILabel?
-    
-    
-    
-    private static var manager: Alamofire.SessionManager = {
-        
-        // Create the server trust policies
-        let serverTrustPolicies: [String: ServerTrustPolicy] = [
-            "aggregation-web.sit.va.anthem.com": .disableEvaluation
-        ]
-        
-        // Create custom manager
-        let configuration = URLSessionConfiguration.default
-        configuration.httpAdditionalHeaders = Alamofire.SessionManager.defaultHTTPHeaders
-        let manager = Alamofire.SessionManager(
-            configuration: URLSessionConfiguration.default,
-            serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies)
-        )
-        
-        return manager
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,21 +25,7 @@ class ViewController: UIViewController {
         usernameLabel?.text = username
         
         // Do any additional setup after loading the view, typically from a nib.
-        let headers: HTTPHeaders = [
-            "X-MADT-AppId": "ABCBS",
-            "X-MADT-AppVersion": "8.0.1200",
-            "Content-Type": "application/json"
-        ]
-        let parameters: Parameters = [
-            "userName": "SIT5SUB340T91519",
-            "password": "support1",
-            "actualUserName": ""
-        ]
-        ViewController.manager.request("https://aggregation-web.sit.va.anthem.com/ma-authentication-app-v6/rest/public/login", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
-            if let json = response.result.value {
-                print("JSON: \(json)") // serialized json response
-            }
-        }
+
     }
 
     override func didReceiveMemoryWarning() {
